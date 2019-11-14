62ac214f79fb5e168551d0df40eb47a7e816edbc
Jonathan R <2140173@gmail.com>
2019-11-14 15:36:42 +0200

    Updates

---
+++ precache-manifest.cd473d941dd0cddf5a4d6410899f8cf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256bf03616b3f75fee3f6b641a211c63",
+    "url": "/Weather_App/index.html"
+  },
+  {
+    "revision": "2f4305e8c0ed501c5cea",
+    "url": "/Weather_App/static/js/2.d30c6b8f.chunk.js"
+  },
+  {
+    "revision": "e9b3ffe4e24ec54238ed",
+    "url": "/Weather_App/static/js/main.f9b0247f.chunk.js"
+  },
+  {
+    "revision": "982fc95d111f15db32fb",
+    "url": "/Weather_App/static/js/runtime~main.d27e29ad.js"
+  },
+  {
+    "revision": "27b2ec5d55ca2394fd2ff22e6eb89559",
+    "url": "/Weather_App/static/media/thumb.27b2ec5d.jpg"
+  }
+]);
